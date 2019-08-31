3482abf5d2a717160b319b4a0850afd240b539ad
Thales Martins <linkthales@hotmail.com>
2019-08-31 00:30:10 -0300

    Added update user endpoint.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -21,8 +21,34 @@ class UserController {
   }
 
   async update(request, response) {
-    console.log(request.userId);
-    return response.json({ ok: true });
+    const { email, oldPassword } = request.body;
+
+    const user = await User.findByPk(request.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return response
+          .status(400)
+          .json({ error: 'E-mail already registered.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return response.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const { id, name, provider } = await user.update(request.body);
+
+    return response.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
