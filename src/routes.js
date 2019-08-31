020a8e963fde0ee67e8ccf98e414515250d3a0d1
Thales Martins <linkthales@hotmail.com>
2019-08-30 23:25:41 -0300

    User controller created.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (request, response) => {
-  return response.json('Lulu');
+routes.get('/', async (request, response) => {
+  return response.json('lulu');
 });
+
+routes.post('/users', UserController.store);
 
 export default routes;
