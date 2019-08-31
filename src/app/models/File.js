53afd3414c3eff0a5cc7c593c2ff59d842d06b4b
Thales Martins <linkthales@hotmail.com>
2019-08-31 14:12:32 -0300

    Added get providers route and static file host on express.

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -6,6 +6,12 @@ class File extends Model {
       {
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://127.0.0.1:3333/files/${this.path}`;
+          },
+        },
       },
       {
         sequelize,
